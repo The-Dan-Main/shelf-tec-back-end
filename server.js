2f3748ba7d976c98aab633dc585d71d650462578
The-Dan-Main <104311440+The-Dan-Main@users.noreply.github.com>
2022-10-17 17:03:55 +0200

    init6

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const app = express()
 const importData = require("./data.json")
+import cors from "cors"
 
+
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("hello")
