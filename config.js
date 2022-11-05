3c6f9521192f9a9d2eba615e4cc0da670fb13365
The-Dan-Main <104311440+The-Dan-Main@users.noreply.github.com>
2022-11-05 21:19:53 +0100

    test r10 fail

--- config.js
+++ config.js
@@ -13,6 +13,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
+    port: process.env.PORT
 })
 
 module.exports = connection;
