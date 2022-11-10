609c4d1866fe489599b0e40330a8ade8c21d2b95
The-Dan-Main <104311440+The-Dan-Main@users.noreply.github.com>
2022-11-10 11:28:37 +0100

    changed auth route

--- config.js
+++ config.js
@@ -1,20 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// example: {
-    // host: 'localhost',
-    // user: 'root',
-    // password: 'Login_2022!',
-    // database: 'shelftec'
-// }
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE
-    // ,
-    // port: process.env.PORT
 })
 
 module.exports = connection;
