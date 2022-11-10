e797820d7698de60e65212917a0935ed89e83d29
The-Dan-Main <104311440+The-Dan-Main@users.noreply.github.com>
2022-11-10 19:24:57 +0100

    testing...

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const app = express()
 const connection = require("./config");
 const secrets = require('./secrets.js');
-// const authRouter = require("./routes/auth.route");
+const authRouter = require("./routes/auth.route");
 const cartRouter = require("./routes/cart.route");
 const ordersRouter = require("./routes/orders.route");
 const productsRouter = require("./routes/products.route");
@@ -38,7 +38,7 @@ app.use(function (req, res, next) {
 
 
 // Routes
-// app.use("/auth", authRouter);
+app.use("/auth", authRouter);
 app.use("/products", productsRouter);
 app.use("/cart", cartRouter);
 app.use("/orders", ordersRouter);
